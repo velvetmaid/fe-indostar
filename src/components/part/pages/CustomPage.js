e8ce365a0db7ad1bc76a03b0424bfd105694402c
camieuxgoofy <tarsisiusadhi@gmail.com>
2022-09-24 16:20:31 +0700

    new adding

--- src/components/part/pages/CustomPage.js
+++ src/components/part/pages/CustomPage.js
@@ -1,23 +1,29 @@
 import React from "react";
 import ImageSlider from "../slider/ImageSlider";
-import { HeroContainer, WrapperFlex } from "../../styled/StyledComponents";
-import { DataImageSlider } from "../pages/data/SliderData";
-import './CustomPage.css'
+import {
+  ContainerWrap,
+  HeroContainer,
+  WrapperFlex,
+} from "../../styled/StyledComponents";
+import { DataSlider } from "../pages/data/SliderData";
+import "./CustomPage.css";
 
 function CustomPage() {
   return (
     <HeroContainer className="container">
-      <WrapperFlex>
-        <div className="description">
-          <h1> Al - Qur 'an Custom Nama</h1>
-          <p>
-            "Dilengkapi dengan berbagai fitur desain yang menarik, menjadikan
-            kita lebih bersemengat dan termotivasi untuk membaca dan menghafal.
-            "
-          </p>
-        </div>
-        <ImageSlider slides={DataImageSlider} />
-      </WrapperFlex>
+      <ContainerWrap>
+        <WrapperFlex className="wrapper">
+          <div className="description">
+            <h1> Al-Qur'an Custom Nama</h1>
+            <p>
+              "Dilengkapi dengan berbagai fitur desain yang menarik, menjadikan
+              kita lebih bersemengat dan termotivasi untuk membaca dan
+              menghafal. "
+            </p>
+          </div>
+          <ImageSlider slides={DataSlider} />
+        </WrapperFlex>
+      </ContainerWrap>
     </HeroContainer>
   );
 }
--- src/components/part/pages/Packet.js
+++ src/components/part/pages/Packet.js
@@ -1,17 +1,75 @@
 import React from "react";
-import { Container, WrapperFlex, Card } from "../../styled/StyledComponents";
+import {
+  Container,
+  WrapperFlex,
+  Card,
+  ContainerWrap,
+} from "../../styled/StyledComponents";
 import "./Packet.css";
 
 function Packet() {
   return (
     <Container className="bg-white">
-      <h1>SAYA MAU PESAN BERAPA HARGANYA?</h1>
-      <h2>Berikut Adalah Harga Al-Qur an Custom Nama</h2>
-      <WrapperFlex>
-        <Card className="bronze"><h1>Paket Bronze</h1></Card>
-        <Card className="gold"><h1>Paket Gold</h1></Card>
-        <Card className="diamond"><h1>Paket Diamond</h1></Card>
-      </WrapperFlex>
+      <ContainerWrap>
+        <h1 className="pkg-heading">SAYA MAU PESAN BERAPA HARGANYA?</h1>
+        <h2 className="pkg-heading">
+          Berikut Adalah Harga Al-Qur an Custom Nama
+        </h2>
+        <WrapperFlex>
+          <Card className="bronze">
+            <h1>Paket Bronze</h1>
+            <span>1 Pcs Al-Qur'an</span>
+            <h1>
+              HARGA<br></br>
+              RP 350.000
+            </h1>
+            <ul>
+              <li>Gratis Custom Nama</li>
+              <li>Gratis Luxury Box</li>
+              <li>Gratis Ucapan</li>
+            </ul>
+          </Card>
+
+          <Card className="gold">
+            <h1>Paket Gold</h1>
+            <span>1 Pcs Al-Qur'an</span>
+            <h1>
+              HARGA<br></br>
+              RP 350.000
+            </h1>
+            <ul>
+              <li>Gratis Custom Nama</li>
+              <li>Gratis Luxury Box</li>
+              <li>Gratis Ucapan</li>
+              <li>
+                <strong>Gratis 1 Al-Qur an Custom Nama</strong>
+              </li>
+            </ul>
+          </Card>
+
+          <Card className="diamond">
+            <h1>Paket Diamond</h1>
+            <span>1 Pcs Al-Qur'an</span>
+            <h1>
+              HARGA<br></br>
+              RP 350.000
+            </h1>
+            <ul>
+              <li>Gratis Custom Nama</li>
+              <li>Gratis Luxury Box</li>
+              <li>Gratis Ucapan</li>
+              <li>
+                <strong>Gratis 1 Al-Qur an Custom Nama</strong>
+              </li>
+              <li>
+                <strong>
+                  Gratis Custom Logo Perusahaan pada cover dan Al-Qur an
+                </strong>
+              </li>
+            </ul>
+          </Card>
+        </WrapperFlex>
+      </ContainerWrap>
     </Container>
   );
 }
