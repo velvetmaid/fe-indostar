cc45439a3fa54444c8d78df75f54df634c251b9c
camieuxgoofy <tarsisiusadhi@gmail.com>
2022-09-26 02:46:24 +0700

    box shadow hide

--- src/components/styled/StyledComponents.js
+++ src/components/styled/StyledComponents.js
@@ -35,7 +35,7 @@ export const WrapperFlex = styled.div`
 
 export const ImgShadow = styled.img`
   max-width: 100%;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  // box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   @media only screen and (max-width: 768px) {
     max-width: 90%;
   }
